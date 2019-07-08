5bdf022487603931928481806dbbe537e6158b99
brent de geest <brentdegeest@live.be>
2019-07-08 10:14:43 +0200

    Text change

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            string Naam = "Naam";
+            Console.WriteLine($"Hello {Naam}!");
         }
     }
 }
